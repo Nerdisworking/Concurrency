a848a29c2731122944646b5874a962d8281a8e60
Nerdisworking <Nerdisworking@gmail.com>
2022-02-14 17:06:36 -0700

    Learn Concurrency

--- src/main/java/com/example/concurrency/SynchronizedBalance.java
+++ src/main/java/com/example/concurrency/SynchronizedBalance.java
@@ -8,7 +8,23 @@ public class SynchronizedBalance {
         return balance;
     }
 
+    /**
+     * public synchronized int getBalance() { return balance; }
+     * public int getBalance() {synchronized(this){ return balance; } }
+     *     Both are Same
+     * **/
+
     public synchronized void addToBalance (int amount) {
         balance +=amount;
     }
+
+    /**
+     * public synchronized void addToBalance (int amount) { balance +=amount;}
+     *
+     * public void addToBalance (int amount) synchronized(this){{ balance +=amount;}}
+     *
+     * Both are same
+     *
+     * synchronized (this){}
+     * **/
 }
